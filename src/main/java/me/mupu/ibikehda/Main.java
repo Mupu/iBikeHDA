545e4cd93640fc1a6da2555fed1295bdb14d6a37
Mupu <Mupuk55@web.de>
2019-02-25 16:37:46 +0100

    added HashPasswordEncoder

--- src/main/java/me/mupu/ibikehda/Main.java
+++ src/main/java/me/mupu/ibikehda/Main.java
@@ -1,12 +1,18 @@
 package me.mupu.ibikehda;
 
 import lombok.Getter;
+import lombok.val;
 import lombok.var;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        val testPW = "test1234";
+        val encoder = new HashPasswordEncoder();
+        val hashedPW = encoder.encode(testPW);
+        System.out.println(testPW);
+        System.out.println(hashedPW);
+        System.out.println(encoder.matches(testPW, hashedPW));
     }
 }
