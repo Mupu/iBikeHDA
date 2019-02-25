23731b879c758c45d18d81ffcaa3025486ba5394
Mupu <Mupuk55@web.de>
2019-02-25 16:14:52 +0100

    added basic structure

--- src/main/java/me/mupu/ibikehda/Main.java
+++ src/main/java/me/mupu/ibikehda/Main.java
@@ -1,6 +1,10 @@
-package me.mupu;
+package me.mupu.ibikehda;
+
+import lombok.Getter;
+import lombok.var;
 
 public class Main {
+
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
